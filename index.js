b8f7910dac22734861527f9380d88ab7b60b98a3
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-04-19 18:34:08 +0600

    new data add

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const arr = [10, 45, 25, 78, 25, 45, 96, 36, 51];
-const sum = arr.reduce(function (prev, cur) {
-  return prev + cur;
-}, 0);
-console.log(sum);
+// const arr = [10, 45, 25, 78, 25, 45, 96, 36, 51];
+// const sum = arr.reduce(function (prev, cur) {
+//   console.log("prev", prev, "current", cur);
+//   return prev + cur;
+// }, 0);
+// console.log(sum);
+
+const arr = [
+  { name: "Someone 0" , salary: 100 }
+  { name: "Someone 1" , salary: 200 }
+  { name: "Someone 2" , salary: 300 }
+  { name: "Someone 3" , salary: 400 }
+  { name: "Someone 4" , salary: 500 }
+];
