4efda12406f3438edc794049838509a6fdd62e1f
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-04-19 19:10:01 +0600

    new data add

--- index.js
+++ index.js
@@ -6,9 +6,20 @@
 // console.log(sum);
 
 const arr = [
-  { name: "Someone 0" , salary: 100 }
-  { name: "Someone 1" , salary: 200 }
-  { name: "Someone 2" , salary: 300 }
-  { name: "Someone 3" , salary: 400 }
-  { name: "Someone 4" , salary: 500 }
+  { name: "Someone 0", salary: 100 },
+  { name: "Someone 1", salary: 200 },
+  { name: "Someone 2", salary: 300 },
+  { name: "Someone 3", salary: 400 },
+  { name: "Someone 4", salary: 500 },
 ];
+const sum = arr.reduce(function (prevous, current) {
+  return prevous + current.salary;
+}, 0);
+// console.log(sum);
+
+const person = {
+  name: "Sakib",
+  age: "28",
+  address: "uttara",
+};
+console.log(person.name, person.age);
