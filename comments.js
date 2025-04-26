71e72168d26afa9ec3f5ffa9b5bd4c9c01abd02a
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-04-26 11:32:24 +0600

    data load async & await use

---
+++ comments.js
@@ -0,0 +1,12 @@
+const loadComments = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log("error happened", error));
+};
+
+const loadComments2 = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+};
