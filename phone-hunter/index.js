e77f6795c30542e719da1ba109e62a8ad6f34bec
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-05-03 20:42:06 +0600

    all card loded

--- phone-hunter/index.js
+++ phone-hunter/index.js
@@ -10,8 +10,31 @@ const loadPhone = async () => {
 
 const displayPhones = (phones) => {
   // console.log(phones);
+  const phoneContainer = document.getElementById("phone-container");
   phones.forEach((phone) => {
     console.log(phone);
+    // 1 create a div
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card bg-gray-100 w-96 shadow-sm`;
+    phoneCard.innerHTML = `
+         <figure>
+              <img
+                src="${phone.image}"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Card Title</h2>
+              <p>
+                A card component has a figure, a body part, and inside body
+                there are title and actions parts
+              </p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+    `;
+    phoneContainer.appendChild(phoneCard);
   });
 };
 
